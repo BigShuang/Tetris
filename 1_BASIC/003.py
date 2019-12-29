3d514d83004eceb32169b42f033072694fc6c1a0
minight2016 <2623028487@qq.com>
2019-12-29 11:06:32 +0800

    修改

--- 1_BASIC/003.py
+++ 1_BASIC/003.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import random
 
 cell_size = 30
 C = 12
@@ -99,22 +100,63 @@ def draw_block_move(canvas, block, direction=[0, 0]):
     # 在新位置绘制新的俄罗斯方块就好
     draw_cells(canvas, x+dx, y+dy, cell_list, SHAPESCOLOR[shape_type])
 
-a_block = {
-    'kind': 'O',  # 对应俄罗斯方块的类型
-    'cell_list': SHAPES['O'],  # 对应俄罗斯方块的各个方格
-    'xy': [3, 3]  # 对应横纵坐标，以左上角为原点，水平向右为x轴正方向，竖直向下为y轴正方向，x对应横坐标，y对应纵坐标
-}
 
-draw_block_move(canvas, a_block)
+def generate_new_block():
+    # 随机生成新的俄罗斯方块
+
+    kind = random.choice(list(SHAPES.keys()))
+    # 对应横纵坐标，以左上角为原点，水平向右为x轴正方向，
+    # 竖直向下为y轴正方向，x对应横坐标，y对应纵坐标
+    xy = [C // 2, 0]
+    new_block = {
+        'kind': kind,  # 对应俄罗斯方块的类型
+        'cell_list': SHAPES[kind],
+        'xy': xy
+    }
+
+    return new_block
+
+
+def check_move(block, direction=[0, 0]):
+    """
+        判断俄罗斯方块是否可以朝制定方向移动
+        :param block: 俄罗斯方块对象
+        :param direction: 俄罗斯方块移动方向
+        :return: boolean 是否可以朝制定方向移动
+        """
+    xy = block['xy']
+    cell_list = block['cell_list']
+
+    if direction[0] or direction[1]:
+        for cell in cell_list:
+            cell_c, cell_r = cell
+            c = cell_c + xy[0] + direction[0]
+            r = cell_r + xy[1] + direction[1]
+            # 判断该位置是否超出左右边界，以及下边界
+            # 一般不判断上边界，因为俄罗斯方块生成的时候，可能有一部分在上边界之上还没有出来
+            if c < 0 or c >= C or r >= R:
+                return False
+
+    return True
 
 
 def game_loop():
     win.update()
-
-    down = [0, 1]
-    draw_block_move(canvas, a_block, down)
+    global current_block
+    if current_block is None:
+        new_block = generate_new_block()
+        # 新生成的俄罗斯方块需要先在生成位置绘制出来
+        draw_block_move(canvas, new_block)
+        current_block = new_block
+    else:
+        if check_move(current_block, [0, 1]):
+            draw_block_move(canvas, current_block, [0, 1])
+        else:
+            current_block = None
 
     win.after(FPS, game_loop)
+
+current_block = None
 
 win.update()
 win.after(FPS, game_loop) # 在FPS 毫秒后调用 game_loop方法
