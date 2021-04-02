96d7800cea0ba52ace65e6f22b3a3f5382478421
BigShuang <3508645145@qq.com>
2021-04-02 11:27:33 +0800

    update ai tetris

--- 3_AI/tetris_by_class.py
+++ 3_AI/tetris_by_class.py
@@ -1,3 +1,18 @@
+"""
+block字典结构:
+{
+    "kind": kind,
+    "cell_list": SHAPES[kind],
+    "cr": [c,r],
+    best: {'cr': (ci, ri),'dc': dc,'angle': angle}
+    'move_steps': [step0,step1,...](step0为列表对象存储字符串,如‘ADW’),     Example:'move_steps': ['WWA', 'AAA', 'A', '']
+    'cur_step': 0(cur_step描述move_steps偏移量,如move_steps[cur_step] == ‘WWA’)
+}
+
+board列表结构:
+board[ri][ci] = ''
+"""
+
 import tkinter as tk
 import random
 
@@ -44,30 +59,33 @@ class Drawer(tk.Canvas):
         super().__init__(master, width=width, height=height)
         self.pack()
 
+    # 初始化（也可以用来重置），用默认色填充画板
     def init(self):
         for ri in range(self.r):
             for ci in range(self.c):
                 self.draw_cell_by_cr(ci, ri, '')
 
-    def draw_cell_by_cr(self, c, r, color, kind=None):
+    # 对指定位置的格子进行涂色(通过坐标及tag_kind，tag_kind决定tags)
+    def draw_cell_by_cr(self, c, r, color, tag_kind=None):
         x0 = c * cell_size
         y0 = r * cell_size
         x1 = c * cell_size + cell_size
         y1 = r * cell_size + cell_size
         # 三种类型
         # 没有俄罗斯方块，None
-        # 失效的俄罗斯方块，dead
+        # 失效的俄罗斯方块，dead(已落地，不能移动的俄罗斯方块)
         # 俄罗斯方块，kind 代表第几个
-        if kind is None:
+        if tag_kind is None:
             self.create_rectangle(x0, y0, x1, y1, fill="#CCCCCC", outline="white", width=2)
-        elif kind == "dead":
+        elif tag_kind == "dead":
             _tag = 'r%s' % r
             self.create_rectangle(x0, y0, x1, y1, fill=color, outline="white", width=2, tags=_tag)
         else:
-            _tag = 'b%s' % kind
+            _tag = 'b%s' % tag_kind
             self.create_rectangle(x0, y0, x1, y1, fill=color, outline="white", width=2, tags=_tag)
 
-    def draw_block(self, block, kind):
+    # 对指定俄罗斯方块进行涂色，颜色由block[kind]决定，kind由函数参数决定
+    def draw_block(self, block, tag_kind):
         c, r = block['cr']
         shape_type = block['kind']
         cell_list = block['cell_list']
@@ -77,13 +95,15 @@ class Drawer(tk.Canvas):
             ri = cell_r + r
             # 判断该位置方格在画板内部(画板外部的方格不再绘制)
             if 0 <= c < C and 0 <= r < R:
-                self.draw_cell_by_cr(ci, ri, SHAPESCOLOR[shape_type], kind)
+                self.draw_cell_by_cr(ci, ri, SHAPESCOLOR[shape_type], tag_kind)
 
+    # 按照block_id删除画板上对应对象
     def clean_by_block_id(self, block_id):
         _tag = 'b%s' % block_id
         self.delete(_tag)
 
-    def clean_by_row(self,r):
+    # 按照行坐标r删除画板上对应对象
+    def clean_by_row(self, r):
         _tag = 'r%s' % r
         self.delete(_tag)
 
@@ -92,26 +112,28 @@ class GameApp:
     def __init__(self, c, r):
         self.win = tk.Tk()
 
-        self.fps = 50
-        self.board = []
-        self.block_list = {}
-        self.block_id = 0
+        self.fps = 50  # 屏幕刷新时间
+        self.board = []  # 二维列表，存放面板上的俄罗斯方块类型信息
+        self.block_list = {}  # block_list存储所有俄罗斯方块对象，key是id， value是block
+        self.block_id = 0  # 对应俄罗斯方块id
         self.running = True
 
         self.c = c
         self.r = r
 
-        self.score = 0
+        self.score = 0 # 总分数
         self.win.title("SCORES: %s" % self.score)
         self.drawer = Drawer(self.win, c, r, cell_size)
         self.drawer.init()
         self.init_board()
 
+    #  类对象初始化时调用，对类变量board列表初始化
     def init_board(self):
-        self.board=[
+        self.board = [
             ['' for ci in range(self.c)] for ri in range(self.r)
         ]
 
+    #  检查是否有可清理的行，有就清理
     def check_and_clear(self):
         has_complete_row = False
         for ri in range(self.r):
@@ -119,23 +141,25 @@ class GameApp:
                 has_complete_row = True
                 # 当前行可消除
                 if ri > 0:
+                    # 当前行为中间行，清理后，上面的行都下落一行
                     for cur_ri in range(ri, 0, -1):
                         self.board[cur_ri] = self.board[cur_ri-1][:]
-                    self.board[0] = ['' for j in range(C)]
+
+                    self.board[0] = ['' for j in range(C)]  # 用新的空行填充首行
                 else:
                     self.board[ri] = ['' for j in range(C)]
 
-                self.score += 10
+                self.score += 10  # 清理一行，进行一次加分
 
         if has_complete_row:
             for r in range(self.r):
-                self.drawer.clean_by_row(r)
+                self.drawer.clean_by_row(r)  # 清除一行（遍历完成后会清理所有行
                 for c in range(self.c):
                     v = self.board[r][c]
                     if v:
                         self.drawer.draw_cell_by_cr(c, r, SHAPESCOLOR[v], 'dead')
 
-            self.win.title("SCORES: %s" % self.score)
+            self.win.title("SCORES: %s" % self.score)  # 更新标题显示的分数
 
     def game_loop(self):
         if self.running:
@@ -154,6 +178,7 @@ class GameApp:
         self.game_loop()
         self.win.mainloop()
 
+    # 对指定方块能否在指定方向进行移动进行判断，返回值为布尔类型
     def check_move(self, cr, cell_list, direction):
         cc, cr = cr
         cell_list = cell_list
@@ -173,6 +198,7 @@ class GameApp:
 
         return True
 
+    # 对当前block_list字典中的block进行单步移动
     def move_block_list(self):
         to_del_id = []
         for block_id in self.block_list:
@@ -217,17 +243,22 @@ class GameApp:
 
         return True
 
+    # 对指定方块在对应列能满足插入的最下方的行ri进行查找并返回，并返回对应的列偏移量dc
+    # dc是为了处理特殊的情况，比如落地后需要左右移动一下的情况
     def get_bottom_r(self, cell_list, ci):
         for ri in range(R-1, -1, -1):
             if self.check_move((ci, ri), cell_list, (0, 0)):
+                # 按顺序检查当前位置与右左位置，上方是否没有阻挡，没有则返回
                 for dc in [0, 1, -1]:
                     nci = ci + dc
                     if self.check_move((nci, ri), cell_list, (0, 0)) and \
                             self.check_above_empty(cell_list, nci, ri):
                         return ri, dc
 
-        raise Exception("no space to place")
+        # raise Exception("no space to place")
+        return -1, -1
 
+    # 检查俄罗斯方块移动到列c后是否越界，返回值为布尔型，True代表可以移动，False表示不能
     def check_col_accessible(self, c, cell_list):
         for cell in cell_list:
             cell_c, cell_r = cell
@@ -237,38 +268,61 @@ class GameApp:
 
         return True
 
+    # 计算当前最佳位置，进行目标cr，angle,br存储，并调用cal_move_order函数对路径进行存储
     def calculate_best_place(self, block):
         shape_type = block['kind']
-
-        index_id = {}
-        index_score = {}
+        index_id = {}  # 存储俄罗斯方块信息键值对
+        index_score = {}  # 存储俄罗斯方块权值键值对
+        # 每一种情况都是一个index，
+        # 理论上，一个俄罗斯方块落地情况，可分为四个角度，c-1个列， 即共 4(c-1)种情况。
         index = 0
         for angle in range(4):
             cell_list = get_cell_list_by_angle(SHAPES[shape_type], angle)
 
             for ci in range(C):
                 if self.check_col_accessible(ci, cell_list):
-                    index += 1
                     ri, dc = self.get_bottom_r(cell_list, ci)
+                    if ri<0: # 这一列放不了东西
+                        continue
 
+                    index += 1
+
+                    # 为每一种场景都复制一个board（深度拷贝），
+                    # 使得当前场景的计算不会影响到其他的
                     cur_board = [row[:] for row in self.board]
                     end_block = {
                         'kind': shape_type,
                         'cell_list': cell_list,
                         'cr': (ci, ri)
                     }
+                    # 将该情景下的俄罗斯方块存储至新建的board中
                     save_block_to_list(end_block,cur_board)
                     index_id[index] = {
                         'cr': (ci, ri),
                         'dc': dc,
                         'angle': angle
                     }
+                    # 计算出当前情况的board权值，并存储到index_score
                     index_score[index] = cal_ai_score(cur_board, self.c, self.r)
 
-        key_name = max(index_score, key=index_score.get)
+        if index == 0: # 没位置可以放，游戏结束
+            self.running = False
+            return
 
-        block['best'] = index_id[key_name]
-        cal_move_order(block)
+        try:
+            key_name = max(index_score, key=index_score.get)  # 得出最高分数的列表角标
+
+            block['best'] = index_id[key_name]  # 对最佳位置坐标，偏移量，角度flag进行存储
+            cal_move_order(block)  # 计算到达最佳位置的路径
+
+            key_name = max(index_score, key=index_score.get)
+
+            block['best'] = index_id[key_name]
+            cal_move_order(block)
+        except Exception as e:
+            self.running = False
+            print("game over with e:", e)
+            print("score:", self.score)
 
 
 game = GameApp(C, R)
